12041a37fef92d2e37b150f00dec537f7bfe781f
Ilnur Saifutdinov <ilnursoft@gmail.com>
2016-03-23 17:19:40 +0300

    fix

--- kaefikbot.go
+++ kaefikbot.go
@@ -36,8 +36,15 @@ func GetToken(namef string) string {
 	res := (strings.Split(fstr, ";"))[0]
 	return res
 }
+
+var (
+	numtoken string
+	reply    string
+	msg      tgbotapi.MessageConfig
+)
+
 func main() {
-	var numtoken string
+
 	fmt.Println("Start kaefik bot...")
 
 	numtoken = GetToken("cfg.token")
@@ -58,8 +65,17 @@ func main() {
 
 	for update := range updates {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		log.Printf("update.Message.Text = %s", update.Message.Text)
+		switch update.Message.Text {
+		case "/start":
+			reply = fmt.Sprintf(`Привет @%s! Я тут слежу за порядком. Веди себя хорошо.`, update.Message.From.UserName)
+		case "/kurs":
+			reply = fmt.Sprintf(`Функционал вывода курсов валют Казани пока в разработке.`)
+		default:
+			reply = update.Message.Text
+		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 		msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
